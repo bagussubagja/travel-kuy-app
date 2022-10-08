67d28cd5ee3086833716ca2f37beb1f9b9796037
Bagus Subagja <bagussubagja17@gmail.com>
2022-10-09 00:44:16 +0700

    Add Maps Location View

--- lib/screens/details/overview_page.dart
+++ lib/screens/details/overview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:readmore/readmore.dart';
 import 'package:travel_kuy_app/models/favorite_model.dart';
 import 'package:travel_kuy_app/models/place_model.dart';
@@ -6,11 +7,27 @@ import 'package:travel_kuy_app/screens/details/widgets/gallery_photo.dart';
 import 'package:travel_kuy_app/shared/theme.dart';
 import 'package:travel_kuy_app/widgets/margin_widget_height.dart';
 import 'package:travel_kuy_app/widgets/margin_widget_width.dart';
+import 'package:map_launcher/map_launcher.dart';
 
-class OverviewPage extends StatelessWidget {
+class OverviewPage extends StatefulWidget {
   OverviewPage({Key? key, this.placeModel, this.favModel}) : super(key: key);
   PlaceModel? placeModel;
   FavoriteModel? favModel;
+
+  @override
+  State<OverviewPage> createState() => _OverviewPageState();
+}
+
+class _OverviewPageState extends State<OverviewPage> {
+  List<String>? coordinateSplitted;
+  String? lang, longitude;
+  @override
+  void initState() {
+    // TODO: implement initState
+    coordinateSplitted = widget.placeModel!.coordinate.split(' ');
+    lang = coordinateSplitted?[0].replaceAll(",", "");
+    longitude = coordinateSplitted?[1].replaceAll(",", "");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +42,9 @@ class OverviewPage extends StatelessWidget {
           ),
           MarginHeight(height: 5),
           ReadMoreText(
-            placeModel?.description == null
-                ? favModel!.description!
-                : placeModel!.description,
+            widget.placeModel?.description == null
+                ? widget.favModel!.description!
+                : widget.placeModel!.description,
             trimLines: 3,
             style: regularText.copyWith(color: greyColor),
             colorClickableText: greenDarkerColor,
@@ -41,7 +58,8 @@ class OverviewPage extends StatelessWidget {
             style: titleText,
           ),
           MarginHeight(height: 10),
-          GalleryPhoto(placeModel: placeModel, favModel: favModel),
+          GalleryPhoto(
+              placeModel: widget.placeModel, favModel: widget.favModel),
           MarginHeight(height: 10),
           Text(
             'Address',
@@ -57,12 +75,32 @@ class OverviewPage extends StatelessWidget {
               MarginWidth(width: 5),
               Flexible(
                 child: Text(
-                  placeModel?.address ?? favModel!.address!,
+                  widget.placeModel?.address ?? widget.favModel!.address!,
                   style: regularText.copyWith(color: greyColor),
                 ),
               ),
             ],
-          )
+          ),
+          MarginHeight(height: 10),
+          Center(
+            child: ElevatedButton(
+                style:
+                    ElevatedButton.styleFrom(backgroundColor: greenDarkerColor),
+                onPressed: () async {
+                  final availableMaps = await MapLauncher.installedMaps;
+                  await availableMaps.first.showMarker(
+                    description: widget.placeModel?.description ??
+                        widget.favModel!.description!,
+                    coords:
+                        Coords(double.parse(lang!), double.parse(longitude!)),
+                    title: widget.placeModel?.name ?? widget.favModel!.name!,
+                  );
+                },
+                child: Text(
+                  "Show on Google Maps",
+                  style: regularText,
+                )),
+          ),
         ],
       ),
     );
