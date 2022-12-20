bfe9e14f596bd32674f46bc79d927349f2a3821c
Bagus Subagja <bagussubagja17@gmail.com>
2022-12-21 00:19:21 +0700

    add http override class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:travel_kuy_app/providers/providers_list.dart';
 import 'package:travel_kuy_app/routes/routes.dart';
 import 'package:travel_kuy_app/shared/theme.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const TravelKuy());
 }
 
